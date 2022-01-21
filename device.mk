6b042ee9aaf0522b1ca9f9782f73d51826da20a0
SebaUbuntu <barezzisebastiano@gmail.com>
2022-01-21 15:17:08 +0000

    selene: Uprev clearkey DRM HIDL to 1.4
    Signed-off-by: Mikazuuu07 <irfn4566@gmail.com>

--- device.mk
+++ device.mk
@@ -160,7 +160,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service-lazy \
-    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.0 \
     android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.1 \
