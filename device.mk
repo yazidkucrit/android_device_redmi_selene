84bcf3b88eec3231bd7a2e4b263a59e7eb8c3042
yazidkucrit <yazid.kucrit@gmail.com>
2022-02-10 20:24:10 +0700

    selene: remove snap add GrapheneOSCamera

--- device.mk
+++ device.mk
@@ -125,7 +125,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
+    GrapheneOSCamera \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4.vendor \
     android.hardware.camera.provider@2.5 \
