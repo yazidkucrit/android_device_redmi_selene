335cde02059831e5ec99c809e775d5c8c493fcde
yazidkucrit <yazid.kucrit@gmail.com>
2022-02-21 09:38:27 +0700

    selene: Don't inherit updatable_apex.mk
    This is because of the following reasons:
    - Many build scripts force disable it anyways by settings OVERRIDE_TARGET_FLATTEN_APEX env flag so why bother rendering differences in official and local builds [1][2].
    - Compressed apex causes issues with OTA on A only devices [3].
    - Compressed apex may cause issues on devices which do not meet kernel requirements [4].
    - Highly anticipated mainline modules update aka Play System updates is limited to OEMs/Pixels only [5], we don't do such things on AOSP; shipping Pixel prebuilt apex components feels somewhat unethical.
    
    Ref:
    [1]: https://github.com/lineageos-infra/build-config/blob/8e435ce7ff7c74097b3500e143380273e099aefb/android/build.sh#L9
    [2]: https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/updatable_apex.mk#L19
    [3]: RecoverySystemService: Failed to reserve space for compressed apex
    [4]: https://source.android.com/devices/tech/ota/apex#kernel-requirements
    [5]: "pushed to end-user devices, either by Google (using the Google Play Store infrastructure) or by the Android partner (using a partner-provided OTA mechanism)." as stated in https://source.android.com/devices/architecture/modular-system#architecture
    
    test: m, boots and ota.
    
    Change-Id: I5b5c2366398791100fa2222ef779ca4d90842211
    Co-Authored-By: ksharma001 <kubersharma001@gmail.com>

--- device.mk
+++ device.mk
@@ -19,9 +19,6 @@ DEVICE_PATH := device/xiaomi/selene
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Inherit virtual_ab_ota product
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
