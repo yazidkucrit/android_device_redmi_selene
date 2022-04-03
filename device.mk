8385e4db8739e2e1a102a4d6086b6a3c5af8b69c
yazidkucrit <yazid.kucrit@gmail.com>
2022-04-03 21:32:28 +0700

    selene: Speed profile services
    Co-Authored-By: Mathieu Chartier <mathieu.a.chartier@gmail.com>

--- device.mk
+++ device.mk
@@ -518,3 +518,6 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0.vendor \
     android.hardware.usb@1.1 \
     android.hardware.usb@1.1.vendor
+	
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
